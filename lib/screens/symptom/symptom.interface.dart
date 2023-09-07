58f36380bc575d965c82dab307b992b28570902a
BZDGEO001 <BZDGEO001@myuct.ac.za>
2023-09-07 12:46:54 +0200

    addind default classes info

---
+++ lib/screens/symptom/symptom.interface.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SymptomsInterface extends StatefulWidget{
+  @override
+  _SymptomsInterfaceState createState() => _SymptomsInterfaceState();
+}
+
+class _SymptomsInterfaceState extends State<SymptomsInterface>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold();
+  }
+}
